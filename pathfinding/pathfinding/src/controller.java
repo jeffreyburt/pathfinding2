1dee2311f06b7e0ffd539c9ece703d6f762d137e
Jeffrey <burtj@catlin.edu>
2021-05-16 15:06:31 -0700

    eeeeee

--- pathfinding/pathfinding/src/controller.java
+++ pathfinding/pathfinding/src/controller.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class controller {
     public static void main(String[] args){
         ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -5,7 +7,14 @@ public class controller {
         String dataFolder = null;
 
         ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+        try {
+            // Set System L&F
+            UIManager.setLookAndFeel(
+                    UIManager.getSystemLookAndFeelClassName());
+        } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            // handle exception
+        }
+        new GUI();
     }
 
 }
